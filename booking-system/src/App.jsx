c7d9083bf7f9eda9342e5f8dc2112e1c51a0b9e1
viraj patwari <114917248+yourviraj@users.noreply.github.com>
2025-05-27 10:38:37 +0530

    Initial commit

---
+++ booking-system/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.css"
+// import Navbar from './Component/Navbar'; 
+import DharamshalaBooking from './Component/DharamshalaList';
+const App = () => {
+  return (
+    <div>
+    <DharamshalaBooking />
+    </div>
+  );
+}
+
+export default App;
